e7e321d06293c6c5fdb3b5cc95f409e089311c6f
Igor Kamrowski <kamryydev@gmail.com>
2023-02-21 16:08:00 +0100

    comment

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
